0af9d84e07214e7f52c9ed963bbbcbbb46c1a42d
abhinath-jadhav <137531738+abhinath-jadhav@users.noreply.github.com>
2024-09-27 22:37:55 +0530

    updated auth

--- functions/api.js
+++ functions/api.js
@@ -40,7 +40,7 @@ router.get("/", (req, res) => {
 router.use("/user", userRouter);
 router.use("/food", foodRouter);
 router.use("/inventory", inventoryRouter);
-router.use("/cart", verifyTokenAndRole(), cartRouter);
+router.use("/cart", cartRouter);
 
 app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
